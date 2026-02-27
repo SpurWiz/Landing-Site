f57ba3e151f7472fce8a92c2a96a87f4c57d83fb
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-02-27 01:30:12 +0100

    worked on brief structure

---
+++ app/career/page.tsx
@@ -0,0 +1,10 @@
+import Wrapper from '@/components/wrapper'
+import React from 'react'
+
+const CareerPage = () => {
+  return (
+    <Wrapper>CareerPage</Wrapper>
+  )
+}
+
+export default CareerPage
