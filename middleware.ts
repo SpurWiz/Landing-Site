4834132b95bc93465540379330cf669a86555549
Oluwasusi Stephen <stephanyemmitty@gmail.com>
2026-04-18 16:08:23 +0100

    pushing update

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ export function middleware(request: NextRequest) {
   const host = request.headers.get('host') || ''
   const url = request.nextUrl.clone()
 
-  if (host.startsWith('legacy.')) {
-    url.pathname = `/legacy${url.pathname}`
+  if (host.startsWith('legacylens.')) {
+    url.pathname = `/legacylens${url.pathname}`
     return NextResponse.rewrite(url)
   }
 
